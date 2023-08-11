bcac1151909dc64e45dfeb3a0724028ae0526f3a
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-10 22:52:04 -0500

    fit lin reg

---
+++ linear.h
@@ -0,0 +1,23 @@
+#include <matrix.h>
+
+class LinearRegression
+{
+private:
+    Vector coefficients;
+
+public:
+    void fit(const Matrix &X, const Vector &y)
+    {
+        Matrix Xt = transpose(X);
+        Matrix XtX = multiply(Xt, X);
+        Matrix XtX_inv = inverse(XtX);
+        Matrix XtX_inv_Xt = multiply(XtX_inv, Xt);
+        Matrix betaMatrix = multiply(XtX_inv_Xt, {y});
+
+        coefficients.clear();
+        for (const auto &row : betaMatrix)
+        {
+            coefficients.push_back(row[0]);
+        }
+    }
+};
