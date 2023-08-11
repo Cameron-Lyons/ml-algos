c1635094d94078487dba2a713ee0f7844cec9ba3
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-10 22:57:08 -0500

    Add a column of 1s for the bias term

--- linear.h
+++ linear.h
@@ -8,8 +8,18 @@ private:
 public:
     void fit(const Matrix &X, const Vector &y)
     {
-        Matrix Xt = transpose(X);
-        Matrix XtX = multiply(Xt, X);
+        // Add a column of 1s for the bias term
+        Matrix X_bias(X.size(), Vector(X[0].size() + 1, 1.0));
+        for (size_t i = 0; i < X.size(); i++)
+        {
+            for (size_t j = 0; j < X[0].size(); j++)
+            {
+                X_bias[i][j + 1] = X[i][j];
+            }
+        }
+
+        Matrix Xt = transpose(X_bias);
+        Matrix XtX = multiply(Xt, X_bias);
         Matrix XtX_inv = inverse(XtX);
         Matrix XtX_inv_Xt = multiply(XtX_inv, Xt);
         Matrix betaMatrix = multiply(XtX_inv_Xt, {y});
@@ -30,5 +40,11 @@ public:
     {
         Vector coeff(coefficients.begin() + 1, coefficients.end());
         return coeff;
+    }
+
+    // The bias is the first coefficient
+    double getBias() const
+    {
+        return coefficients[0];
     }
 };
