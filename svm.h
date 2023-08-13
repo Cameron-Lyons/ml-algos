cfc3aa69cca586ebad03edbde609861d9db57884
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-13 14:15:53 -0500

    predict

--- svm.h
+++ svm.h
@@ -5,3 +5,17 @@ private:
     Vector weights;
     double learningRate;
     int maxIterations;
+
+public:
+    Perceptron(int n_features, double learningRate = 0.01, int maxIterations = 1000)
+        : learningRate(learningRate), maxIterations(maxIterations) {
+        weights.resize(n_features, 0.0);
+    }
+
+    int predict(const Vector &x) {
+        double dotProduct = 0.0;
+        for (size_t i = 0; i < x.size(); i++) {
+            dotProduct += x[i] * weights[i];
+        }
+        return (dotProduct >= 0.0) ? 1 : -1;
+    }
