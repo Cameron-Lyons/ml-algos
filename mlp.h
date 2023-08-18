4b9d48a39694c596b9c3737f8c0fb2c024812e8e
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-18 07:15:45 -0500

    apply function

--- mlp.h
+++ mlp.h
@@ -8,3 +8,26 @@ double sigmoid(double x)
 {
     return 1.0 / (1.0 + std::exp(-x));
 }
+
+Vector applyFunction(const Vector &vec, double (*func)(double))
+{
+    Vector result(vec.size());
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        result[i] = func(vec[i]);
+    }
+    return result;
+}
+
+Matrix applyFunction(const Matrix &mat, double (*func)(double))
+{
+    Matrix result(mat.size(), Vector(mat[0].size()));
+    for (size_t i = 0; i < mat.size(); i++)
+    {
+        for (size_t j = 0; j < mat[0].size(); j++)
+        {
+            result[i][j] = func(mat[i][j]);
+        }
+    }
+    return result;
+}
