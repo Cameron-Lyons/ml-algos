d234b1f0c7b53441ddeea47a35aa259e76482563
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-24 22:54:16 -0500

    relu

--- mlp.h
+++ mlp.h
@@ -9,6 +9,11 @@ double sigmoid(double x)
     return 1.0 / (1.0 + std::exp(-x));
 }
 
+double relu(double x)
+{
+    return x > 0 ? x : 0.0;
+}
+
 Vector applyFunction(const Vector &vec, double (*func)(double))
 {
     Vector result(vec.size());
