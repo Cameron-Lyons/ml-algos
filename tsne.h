b772b22dea40713453f561fc5c08bf29982cd460
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-26 16:03:29 -0500

    lowdim

--- tsne.h
+++ tsne.h
@@ -7,3 +7,8 @@ double highDimAffinity(const Point &xi, const Point &xj, double sigma) {
   double distance = euclideanDistance(xi, xj);
   return exp(-distance * distance / (2.0 * sigma * sigma));
 }
+
+double lowDimAffinity(const Point &yi, const Point &yj) {
+  double distance = euclideanDistance(yi, yj);
+  return (1.0 + distance * distance) / pow(1.0 + distance * distance, 2);
+}
