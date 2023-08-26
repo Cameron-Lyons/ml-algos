ae0a61029d2347fe15d1149f4f2ac23f0619309a
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-26 16:17:08 -0500

    computegrad

--- tsne.h
+++ tsne.h
@@ -12,3 +12,17 @@ double lowDimAffinity(const Point &yi, const Point &yj) {
   double distance = euclideanDistance(yi, yj);
   return (1.0 + distance * distance) / pow(1.0 + distance * distance, 2);
 }
+
+double computeGradient(const Points &X, const Points &Y, size_t i, size_t d,
+                       double sigma) {
+  double grad = 0.0;
+  for (size_t j = 0; j < X.size(); j++) {
+    if (i != j) {
+      double p = highDimAffinity(X[i], X[j], sigma);
+      double q = lowDimAffinity(Y[i], Y[j]);
+      grad +=
+          (p - q) * (Y[i][d] - Y[j][d]) * (1.0 + euclideanDistance(Y[i], Y[j]));
+    }
+  }
+  return 2.0 * (1.0 - lowDimAffinity(Y[i], Y[i])) * grad;
+}
