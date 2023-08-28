49c6eb5621b423f45928eb68b75042b1ffeef610
Cameron Lyons <cameron.lyons2@gmail.com>
2023-08-28 13:11:44 -0500

    predict

--- supervised/gaussian_process.h
+++ supervised/gaussian_process.h
@@ -8,7 +8,7 @@ private:
   Matrix X_train;
   Vector y_train;
 
-  double rbf_kernel(const vector<double> &x1, const vector<double> &x2) const {
+  double rbf_kernel(const Vector &x1, const Vector &x2) const {
     double sum = 0.0;
     for (size_t i = 0; i < x1.size(); ++i) {
       sum += (x1[i] - x2[i]) * (x1[i] - x2[i]);
@@ -23,3 +23,17 @@ public:
     X_train = X;
     y_train = y;
   }
+  double predict(const Vector &X_test) {
+    double K_star = 0;
+    double sum = 0.0;
+
+    for (size_t i = 0; i < X_train.size(); ++i) {
+      K_star = rbf_kernel(X_test, X_train[i]);
+      sum +=
+          K_star * y_train[i]; // Assuming the inverse kernel matrix times the
+                               // training outputs is identity for simplicity.
+    }
+
+    return sum;
+  }
+};
